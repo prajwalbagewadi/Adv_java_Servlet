019d5fe6343448666b262ded7899cbc8243f1958
prajwalbagewadi <bagewadi.prajwal44@gmail.com>
2025-02-24 09:02:06 +0530

    login module

--- LoginModule/src/main/java/loginpack/ULogin.java
+++ LoginModule/src/main/java/loginpack/ULogin.java
@@ -15,15 +15,16 @@ import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.sql.ResultSet;
 
+//import com.mysql.cj.jdbc.Driver;
+
 /**
  * Servlet implementation class ULogin
  */
 @WebServlet("/ULogin")
 public class ULogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	public Connection con;
-	public Statement st;
-	public ResultSet rs; 
+	String userstr;
+	String passstr;
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -36,33 +37,51 @@ public class ULogin extends HttpServlet {
    public void init() throws ServletException {
 	   // TODO Auto-generated method stub
 	   super.init();
-	   try {
-	   Class.forName("com.mysql.cj.jdbc.Driver");
-	   Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/login","root","root");
-	   Statement st = con.createStatement();
-	   ResultSet rs  = st.executeQuery("select * from users");
-	   while(rs.next()) {
-		   System.out.println("r= "+rs.getInt(0)+rs.getString(1)+rs.getString(2));
-	   }
-	   }catch (SQLException e) {
-		   e.printStackTrace();
-	   }catch(ClassNotFoundException e) {
-		   e.printStackTrace();
-	   }
+	  
    }
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html"); // resp.setContentType use to define response content format.
-    	PrintWriter out = response.getWriter(); //PrintWriter used insert content on html page. -> resp.getWriter() creates a html file to write.
-    	out.println("welcome to Login Page."); //normal string
-    	//out.println("<em>Prajwal</em>"); //html element
-    	out.println("<form method=\"GET\">"); //html element
-    	out.println("<input type=\"text\" name=\"username\" placeholder=\"enter username\"/>"); //html element
-    	out.println("<input type=\"password\" name=\"password\" placeholder=\"enter password\"/>"); //html element
-    	out.println("<input type=\"submit\" name=\"submit\" value=\"Login\"/>"); //html element
-    	out.println("</form>"); //html element
+		try {
+			   Class.forName("com.mysql.cj.jdbc.Driver");
+			   Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/login","root","root");
+			   Statement st = con.createStatement();
+			   ResultSet rs  = st.executeQuery("select * from users");
+			   while(rs.next()) {
+				   userstr=rs.getString(2);
+				   passstr=rs.getString(3);
+				   System.out.println("r= "+rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
+			   }
+			   response.setContentType("text/html"); // resp.setContentType use to define response content format.
+		    	PrintWriter out = response.getWriter(); //PrintWriter used insert content on html page. -> resp.getWriter() creates a html file to write.
+		    	out.println("welcome to Login Page."); //normal string
+		    	//out.println("<em>Prajwal</em>"); //html element
+		    	out.println("<form method=\"GET\">"); //html element
+		    	out.println("<input type=\"text\" name=\"username\" placeholder=\"enter username\"/>"); //html element
+		    	out.println("<input type=\"password\" name=\"password\" placeholder=\"enter password\"/>"); //html element
+		    	out.println("<input type=\"submit\" name=\"submit\" value=\"Login\"/>"); //html element
+		    	out.println("</form>"); //html element
+		    	
+		    	String reqval= request.getParameter("submit");
+		    	if(reqval!=null) {
+		    		String uname=request.getParameter("username");
+		    		System.out.println("user="+uname);
+		    		String upass=request.getParameter("password");
+		    		System.out.println("pass="+upass);
+		    		
+		    		if(uname.equals(userstr) && upass.equals(passstr)) {
+			    		out.println("userFound in Database");
+	        		}
+		    			
+		    	}
+		    	//con.close();
+		}catch(ClassNotFoundException e) {
+			   e.printStackTrace();
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+	
 	}
 	private void destory() {
 		// TODO Auto-generated method stub
